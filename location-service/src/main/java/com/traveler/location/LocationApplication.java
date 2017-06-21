9598540ea0ded4195ce229f1fcc4ebb78799d41f
Yuzhou Xu <yuzhou.xu@Yuzhous-MacBook-Pro.local>
2017-06-21 00:47:02 -0400

    Bootstrap api gateway

--- location-service/src/main/java/com/traveler/location/LocationApplication.java
+++ location-service/src/main/java/com/traveler/location/LocationApplication.java
@@ -2,12 +2,6 @@ package com.traveler.location;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-import com.traveler.location.domain.Location;
-import com.traveler.location.domain.Size;
-import com.traveler.location.domain.Category;
-import com.traveler.location.repository.LocationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
