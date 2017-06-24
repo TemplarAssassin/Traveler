284ff10f0099188fa9f1ba8ce5a77343f8967675
Yuzhou Xu <yuzhou.xu@Yuzhous-MacBook-Pro.local>
2017-06-24 19:20:20 -0400

    Enable AWS RDS connection to location-service

--- location-service/src/main/java/com/traveler/location/LocationApplication.java
+++ location-service/src/main/java/com/traveler/location/LocationApplication.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRdsInstance(dbInstanceIdentifier = "${cloud.aws.rds.dbInstanceIdentifier}", password="${cloud.aws.rds.springaws.password}")
 @Configuration
 public class LocationApplication{
 
